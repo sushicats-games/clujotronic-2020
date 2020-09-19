0679a839139532d5db50698a1ed9c7f44595767b
Ana Spoiala <ana.spoiala25@gmail.com>
2020-09-19 17:54:14 +0300

    added toolbox UI and implementex tool picking

---
+++ OnTheSafeSide/Assets/Scripts/Tool.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class Tool : MonoBehaviour
+{
+    [SerializeField] private string _name;
+    [SerializeField] private string _type;
+    [SerializeField] private bool _isCorner;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private Sprite icon;
+
+    public string Name { get => _name; set => _name = value; }
+    public string Type { get => _type; set => _type = value; } // wall, floor, roof, foliageanddecor
+    public bool IsCorner { get => _isCorner; set => _isCorner = value; }
+    public GameObject Prefab { get => _prefab; set => _prefab = value; }
+    public Sprite Icon { get => icon; set => icon = value; }
+}
---
+++ OnTheSafeSide/Assets/Scripts/ToolPicker.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolPicker : MonoBehaviour
+{
+    [SerializeField] private List<Tool> _tools;
+    [SerializeField] private GameObject _toolsContainer;
+    [SerializeField] private GameObject _buttonPrefab;
+
+
+    private Tool _currentTool = null;
+
+    public List<Tool> GetAvailableTools(string type = null)
+    {
+        if (string.IsNullOrEmpty(type))
+        {
+            return _tools.ToList();
+        }
+
+        return _tools.Where(tool => tool.Type == type).ToList();
+    }
+
+    public Tool GetTool(string name)
+    {
+        return _tools.SingleOrDefault(tool => tool.Name == name);
+    }
+
+    public Tool GetPickedTool(string name)
+    {
+        return _currentTool;
+    }
+
+    public void PickTool(string name)
+    {
+        _currentTool = GetTool(name);
+    }
+
+    private void _clearToolsContainer()
+    {
+        foreach (Transform child in _toolsContainer.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    #region Event Listeners
+    public void OnTypeButtonClicked(string type)
+    {
+        _clearToolsContainer();
+
+        var filteredTools = GetAvailableTools(type);
+
+        foreach (var tool in filteredTools)
+        {
+            GameObject obj = Instantiate(_buttonPrefab, _toolsContainer.transform);
+            obj.name = tool.name;
+            Button btn = obj.GetComponent<Button>();
+            btn.onClick.AddListener(() => OnToolClicked(tool.Name));
+        }
+    }
+
+    public void OnToolClicked(string name)
+    {
+        PickTool(name);
+    }
+    #endregion
+
+}
